9ba6b4ad426336831198049c1e2a11fbe0f792b4
seungchan.m <m3252@naver.com>
2025-10-06 14:30:14 +0900

    판매자 회원 가입 기능을 위한 CreateSellerCommand 및 POST 요청 테스트 추가

---
+++ src/test/java/test/commerce/api/seller/signup/POST_specs.java
@@ -0,0 +1,41 @@
+package test.commerce.api.seller.signup;
+
+import commerce.CommerceApiApp;
+import commerce.command.CreateSellerCommand;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(
+    classes = CommerceApiApp.class,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@DisplayName("POST /api/sellers/signUp")
+public class POST_specs {
+
+    @Test
+    @DisplayName("올바르게 요청하면 204 No Content 상태코드를 반환한다")
+    void test1(@Autowired TestRestTemplate client) {
+        // Arrange
+        CreateSellerCommand command = new CreateSellerCommand(
+            "seller@test.com",
+            "seller",
+            "password"
+        );
+
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/sellers/signUp",
+            command,
+            Void.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(204);
+    }
+}
